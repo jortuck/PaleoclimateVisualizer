a5b7c0c807a2a896e7c16975ca3311d2e6f0436e
jordan <jordan@driedsponge.net>
2024-07-03 11:54:59 -0700

    update meta description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -82,10 +82,10 @@
 </script>
 <svelte:head>
 	<title>Paleoclimate Visualizer</title>
-	<meta name="description" content="Paleoclimate Visualizer">
+	<meta name="description" content="Explore interactive visualizations of paleoclimate data reconstructions. Dive into the history of Earth's climate through detailed charts and maps.">
 	<meta name="keywords" content="Paleoclimate, Climate, Data Visualization">
 	<meta property="og:title" content="Paleoclimate Visualizer">
-	<meta property="og:description" content="Interact, ">
+	<meta property="og:description" content="Explore interactive visualizations of paleoclimate data reconstructions. Dive into the history of Earth's climate through detailed charts and maps.">
 	<meta property="og:type" content="website">
 	<meta property="og:site_name" content="Paleoclimate Visualizer">
 	<meta property="og:locale" content="en_US">
