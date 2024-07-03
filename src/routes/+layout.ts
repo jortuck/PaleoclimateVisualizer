3c7bdf58f6773d984b5cfc9a454c034dde82d188
jordan <jordan@driedsponge.net>
2024-07-03 09:35:11 -0700

    Add code from previous scratch project, update to use svelte 5 conventions

---
+++ src/routes/+layout.ts
@@ -0,0 +1,2 @@
+// Tells the compiler we plan on having a pre-rendered site that is not dynamic
+export const prerender = true;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,96 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import Highcharts from 'highcharts/highmaps';
+	import GeoHeatmap from "highcharts/modules/geoheatmap"
+	import {csv, json } from "d3"
+
+	let title:string = $state("cesm");
+	let map: any;
+	let chart: Highcharts.MapChart;
+
+	onMount(async ()=>{
+		const topology = await fetch(
+			'https://code.highcharts.com/mapdata/custom/world-continents.topo.json'
+		).then(response => response.json())
+		const antarctica = await fetch(
+			'https://code.highcharts.com/mapdata/custom/antarctica.topo.json'
+		).then(response => response.json());
+		GeoHeatmap(Highcharts)
+
+		let ces = await json(`./data/${title}_psl_trends.json`);
+		// @ts-ignore
+		chart =  Highcharts.mapChart(map,{
+			// @ts-ignore
+			chart:{
+				map:topology,
+			},
+			title:{text:title},
+			series:[{
+				type:"geoheatmap",
+				data:ces,
+				cursor:"crosshair",
+				states:{
+					hover:{
+						enabled: false
+					},
+					inactive: { opacity: 1 },
+				},
+				interpolation: {enabled: true},
+				zIndex:0,
+			},{
+				mapData:antarctica,
+				zIndex:2,
+				color:"#000",
+				states:{
+					inactive: { opacity: 1 },
+				},
+			},{
+				zIndex:2,
+				states:{
+					inactive: { opacity: 1 },
+				},
+			}],
+			mapView:{
+				projection:{
+					rotation: [180, 0, 0]
+				}
+			},
+			mapNavigation:{
+				enabled:true,
+			},
+			colorAxis: {
+				min: -2.5,
+				max: 2.5,
+				stops: [
+					[0, 'rgba(21, 0, 255,0.9)'],
+					[1, 'rgba(255, 0, 0,0.9)']
+				],
+				labels: {
+					style: {
+						color: '#ddd'
+					}
+				}
+			},
+		});
+	})
+	async function updateDataSet(){
+		chart.title.update({text:title+" reconstruction trends"})
+		let newData = await fetch("./data/"+title+"_psl_trends.json").then(response => response.json())
+		// @ts-ignore
+		chart.series[0].update({data:newData})
+	}
+</script>
+<svelte:head>
+	<title>Paleoclimate Visualizer</title>
+</svelte:head>
+<h1>Paleoclimate Visualizer</h1>
+<select bind:value={title} onchange={updateDataSet}>
+	<option value="cesm">CESM</option>
+	<option value="hadcm3">HADCM3</option>
+	<option value="lens">LENS</option>
+	<option value="pace">PACE</option>
+</select>
+<div bind:this={map}>
+
+</div>
+
