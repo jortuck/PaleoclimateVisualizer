6de2ed67db6b94f1d2177b12413cc1496c312423
Jordan Tucker <jordan@jortuck.com>
2025-07-20 16:09:24 -0700

    adjust tab width

--- src/lib/components/Controller/Controller.svelte
+++ src/lib/components/Controller/Controller.svelte
@@ -14,9 +14,9 @@
 </script>
 <div class="space-y-4">
 	<h2 class="text-2xl font-bold">Settings</h2>
-	<div role="tablist" class="tabs tabs-border w-full">
-		<button onclick={()=>{activeTab='data'}} value="data" role="tab" class="tab" class:tab-active={activeTab==='data'}>Data</button>
-		<button onclick={()=>{activeTab='view'}} value="view" role="tab" class="tab" class:tab-active={activeTab==='view'}>View</button>
+	<div role="tablist" class="tabs tabs-border flex w-full">
+		<button onclick={()=>{activeTab='data'}} value="data" role="tab" class="tab flex-1" class:tab-active={activeTab==='data'}>Data</button>
+		<button onclick={()=>{activeTab='view'}} value="view" role="tab" class="tab flex-1" class:tab-active={activeTab==='view'}>View</button>
 		<button onclick={()=>{activeTab='download'}} value="download" role="tab" class="tab hidden" class:tab-active={activeTab==='download'}>Download</button>
 	</div>
 	<div class="flex flex-col space-y-3">
