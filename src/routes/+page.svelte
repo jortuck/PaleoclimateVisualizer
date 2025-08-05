225c1b0790e06c8673436a2154b74d17486debde
Jordan Tucker <jordan@jortuck.com>
2025-08-04 20:12:56 -0700

    reformat

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import Controller from '$lib/components/Controller/Controller.svelte';
 	import Map from '$lib/components/Map.svelte';
 	import { controller } from '$lib/ControllerState.svelte';
-	import type { AvailableDataResponse, MapData, TimeSeriesData } from '$lib/Data';
+	import type { AvailableDataResponse } from '$lib/Data';
 	import { PUBLIC_API_HOST } from '$env/static/public';
 	import { DataController } from '$lib/DataController.svelte';
 	import TimeSeries from '$lib/components/TimeSeries.svelte';
@@ -13,9 +13,11 @@
 		let data = await request.json();
 		return new DataController(data as AvailableDataResponse);
 	}
-	async function setDataController(){
+
+	async function setDataController() {
 		dataController = await getAvailable();
 	}
+
 	let dataController: DataController | null = $state(null);
 	onMount(async () => {
 		dataController = await getAvailable();
@@ -39,15 +41,15 @@
 	<meta property="og:locale" content="en_US" />
 </svelte:head>
 <svelte:boundary>
-		{#await setDataController()}
-			<div
-				class="h-full w-full z-50 bg-base-300 absolute top-0 left-0 opacity-80 flex items-center justify-center flex-col space-y-5"
-			>
-				<h2 class="text-2xl font-bold text-base-content">Loading</h2>
-				<span class="loading loading-spinner loading-lg"></span>
-			</div>
-		{:then value}
-			<div class="grid grid-cols-12 grow">
+	{#await setDataController()}
+		<div
+			class="h-full w-full z-50 bg-base-300 absolute top-0 left-0 opacity-80 flex items-center justify-center flex-col space-y-5"
+		>
+			<h2 class="text-2xl font-bold text-base-content">Loading</h2>
+			<span class="loading loading-spinner loading-lg"></span>
+		</div>
+	{:then value}
+		<div class="grid grid-cols-12 grow">
 
 			<aside class="p-5 bg-base-200 hidden lg:block lg:col-span-4 xl:col-span-3">
 				<Controller controller={dataController} />
@@ -71,12 +73,12 @@
 					class="row-span-6 lg:row-span-5"
 				/>
 			</div>
-			</div>
+		</div>
 
-		{:catch error}
-			<div class="p-10 space-y-8">
-				<div role="alert" class="alert alert-error alert-outline w-full text-lg">
-					<p>
+	{:catch error}
+		<div class="p-10 space-y-8">
+			<div role="alert" class="alert alert-error alert-outline w-full text-lg">
+				<p>
 					<span>
 						The API that this web application relies on cannot be reached. Please visit the <a
 						target="_blank"
@@ -84,12 +86,12 @@
 						href="https://status.jortuck.com/status/pv">status page</a
 					> for updates.
 					</span>
-						<br />
-						<span>Error Message: {error}</span>
-					</p>
-				</div>
+					<br />
+					<span>Error Message: {error}</span>
+				</p>
 			</div>
-			{/await}
+		</div>
+	{/await}
 	<dialog bind:this={controller.modal} id="controllerModal" class="modal lg:hidden">
 		<div class="modal-box bg-base-300">
 			<form method="dialog">
@@ -114,10 +116,10 @@
 				<p>
 					<span>
 						The API that this web application relies on cannot be reached. Please visit the <a
-							target="_blank"
-							class="underline"
-							href="https://status.jortuck.com/status/pv">status page</a
-						> for updates.
+						target="_blank"
+						class="underline"
+						href="https://status.jortuck.com/status/pv">status page</a
+					> for updates.
 					</span>
 					<br />
 					<span>Error Message: {error}</span>
@@ -128,20 +130,22 @@
 </svelte:boundary>
 
 <style lang="postcss">
-	@reference "$lib/app.css";
-	:global(
+    @reference "$lib/app.css";
+    :global(
 		.highcharts-title,
 		.highcharts-axis-labels > span,
 		.highcharts-axis-title,
 		.highcharts-legend-item > span
 	) {
-		@apply !text-base-content lg:text-left text-center;
-	}
-	:global(.highcharts-container) {
-		width: 100% !important;
-		height: 100% !important;
-	}
-	:global(.highcharts-label.highcharts-legend-title > span) {
-		@apply dark:!text-white;
-	}
+        @apply !text-base-content lg:text-left text-center;
+    }
+
+    :global(.highcharts-container) {
+        width: 100% !important;
+        height: 100% !important;
+    }
+
+    :global(.highcharts-label.highcharts-legend-title > span) {
+        @apply dark:!text-white;
+    }
 </style>
