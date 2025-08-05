8e43a5b8009ca7580a19a6c17325118c1e33be91
Jordan Tucker <jordan@jortuck.com>
2025-08-05 14:11:01 -0700

    download buttons on front end

--- src/lib/components/Controller/Controller.svelte
+++ src/lib/components/Controller/Controller.svelte
@@ -3,24 +3,54 @@
 	import ViewControls from '$lib/components/Controller/ViewControls.svelte';
 	import DataControls from '$lib/components/Controller/DataControls.svelte';
 	import type { DataController } from '$lib/DataController.svelte';
+	import DownloadControls from '$lib/components/Controller/DownloadControls.svelte';
 
-	let { controller}: {
-		controller: DataController
+	let {
+		controller
+	}: {
+		controller: DataController;
 	} = $props();
-	let activeTab: "data"|"view"|"download" = $state("data")
+	let activeTab: 'data' | 'view' | 'download' = $state('data');
 </script>
+
 <div class="space-y-4">
 	<h2 class="text-2xl font-bold">Settings</h2>
-	<div role="tablist" class="tabs tabs-border flex w-full">
-		<button onclick={()=>{activeTab='data'}} value="data" role="tab" class="tab flex-1" class:tab-active={activeTab==='data'}>Data</button>
-		<button onclick={()=>{activeTab='view'}} value="view" role="tab" class="tab flex-1" class:tab-active={activeTab==='view'}>View</button>
-		<button onclick={()=>{activeTab='download'}} value="download" role="tab" class="tab hidden" class:tab-active={activeTab==='download'}>Download</button>
+	<div role="tablist" class="tabs tabs-box bg-base-300 flex w-full">
+		<button
+			onclick={() => {
+				activeTab = 'data';
+			}}
+			value="data"
+			role="tab"
+			class="tab flex-1"
+			class:tab-active={activeTab === 'data'}>Data</button
+		>
+		<button
+			onclick={() => {
+				activeTab = 'view';
+			}}
+			value="view"
+			role="tab"
+			class="tab flex-1"
+			class:tab-active={activeTab === 'view'}>View</button
+		>
+		<button
+			onclick={() => {
+				activeTab = 'download';
+			}}
+			value="download"
+			role="tab"
+			class="tab flex-1"
+			class:tab-active={activeTab === 'download'}>Download</button
+		>
 	</div>
 	<div class="flex flex-col space-y-3">
-		{#if activeTab==='view'}
+		{#if activeTab === 'view'}
 			<ViewControls />
-			{:else if activeTab==='data'}
-				<DataControls {controller} />
+		{:else if activeTab === 'data'}
+			<DataControls {controller} />
+		{:else if activeTab === 'download'}
+			<DownloadControls {controller} />
 		{/if}
 	</div>
 </div>
---
+++ src/lib/components/Controller/DownloadControls.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type { DataController } from '$lib/DataController.svelte';
+
+	let {
+		controller
+	}: {
+		controller: DataController;
+	} = $props();
+	let fullUrl: string = $derived(controller.trendUrl + '&download=full');
+	let partialUrl: string = $derived(controller.trendUrl + '&download=partial');
+	let trendUrl: string = $derived(controller.trendUrl + '&download=trend');
+</script>
+
+<p>
+	Below are the download options for the <strong>{controller.currentDataset.name}</strong>
+	reconstruction. Switch back to the <strong>Data</strong> tab to change reconstructions.
+</p>
+<a href={fullUrl} download class="btn btn-primary">Full Reconstruction</a>
+<a href={partialUrl} download class="btn btn-primary"
+	>Partial Reconstruction ({controller.startYear} - {controller.endYear})</a
+>
+<a href={trendUrl} download class="btn btn-primary"
+	>Pre-Calculated Trends ({controller.startYear} - {controller.endYear})</a
+>
