b70556d7e0c020e1a96693352ff65873852504e6
Jordan Tucker <jordan@jortuck.com>
2025-08-11 11:57:08 -0700

    allow for the selection of anomaly reference periods

--- src/lib/components/Controller/DownloadControls.svelte
+++ src/lib/components/Controller/DownloadControls.svelte
@@ -7,15 +7,28 @@
 		controller: DataController;
 	} = $props();
 	let timeseriesFormat: 'csv' | 'mat' | 'xls' = $state('csv');
+	let move_reference: boolean = $state(true);
 	let timeSeriesUrl: string = $derived(
 		controller.timeSeriesUrl +
-			`&download=${timeseriesFormat}&startYear=${controller.startYear}&endYear=${controller.endYear}`
+			`&download=${timeseriesFormat}&startYear=${controller.startYear}&endYear=${controller.endYear}&move_reference=${move_reference}`
 	);
-	let fullUrl: string = $derived(controller.trendUrl + '&download=full');
-	let partialUrl: string = $derived(controller.trendUrl + '&download=partial');
+	let fullUrl: string = $derived(
+		controller.trendUrl + '&download=full&move_reference=' + move_reference
+	);
+	let partialUrl: string = $derived(
+		controller.trendUrl + '&download=partial&move_reference=' + move_reference
+	);
 	let trendUrl: string = $derived(controller.trendUrl + '&download=trend');
 </script>
 
+<p>
+	Please select your desired anomaly reference period. This reference period will apply to all
+	downloads.
+</p>
+<select class="select" bind:value={move_reference}>
+	<option value={false}>1950-1980</option>
+	<option value={true}>1979-2005</option>
+</select>
 <p>
 	Below are the download options for the <strong>{controller.currentDataset.name}</strong>
 	reconstruction. Switch back to the <strong>Data</strong> tab to change reconstructions.
@@ -29,7 +42,13 @@
 >
 
 <p>
-	Below are the download options for downloading the time series {controller.startYear} - {controller.endYear}.
+	Below are the download options for downloading the time series {controller.startYear} - {controller.endYear}
+	for the
+	{#if controller.timeSeriesMode === 'point'}
+		point ({controller.timeSeriesPoint.lat},{controller.timeSeriesPoint.lon}).
+	{:else}
+		area ({controller.area.n},{controller.area.s},{controller.area.start},{controller.area.stop}).
+	{/if}
 </p>
 <div class="join">
 	<select class="select join-item" bind:value={timeseriesFormat}>
